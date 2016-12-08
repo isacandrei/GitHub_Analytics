c150ca84e8da08b9f9b75e44fe4db81d6be548eb
isacandrei <isacandrei19@gmail.com>
2016-12-09 00:09:21 +0100

    linear regresion test - mllib

--- spark-app/project/Build.scala
+++ spark-app/project/Build.scala
@@ -5,13 +5,16 @@ import com.typesafe.sbt.SbtNativePackager._
 import com.typesafe.sbt.packager.Keys._
 
 object ScalaBuild extends Build {
+    val sparkVersion = "2.0.2"
+
   lazy val sparkappMain = mainProject("spark-app-root")
     .aggregate(sparkappProject)
 
   lazy val sparkappProject = defineProject(scalaProject, "spark-app") settings (
     mainClass in Compile := Some("rugds.sparkapp.SparkappMain"),
     libraryDependencies ++= Seq(
-        "org.apache.spark" %% "spark-core" % "2.0.2"
+        "org.apache.spark" %% "spark-core" % sparkVersion,
+        "org.apache.spark"  % "spark-mllib_2.11" % sparkVersion
     )
   )
 }
